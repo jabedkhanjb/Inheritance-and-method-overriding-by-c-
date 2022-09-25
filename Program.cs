488b1c14db1eb891d5b6745f2c28a6249dff5793
jabedkhanjb <mahfuzislam1410@gmail.com>
2022-09-25 13:57:08 +0600

    inheritance and method overriding

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+abstract class Vehicle{public abstract void vehiclevarrient();
+public void Vehiclecolor(){Console.WriteLine("Color must be Grey");} 
+}
+class Car : Vehicle{public override void vehiclevarrient()
+    {Console.WriteLine("Toyota Fortuna 2020 Edition");}
+}
+class Program
+{static void Main()
+    {
+        Car info = new Car();
+        info.vehiclevarrient();
+        info.Vehiclecolor();
+    }
+}
